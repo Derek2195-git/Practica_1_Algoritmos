6942c89bfff803b88a5568b2add5eae28e019f96
Derek2195-git <derek.garzon@uabc.edu.mx>
2026-09-01 21:13:03 -0700

    Más añadidos a las clases de la terminal - BlackjackGame esta al 60% - Jugador esta 100% terminado - Mano esta 90% acabada - Se renombró VistaBlackJack a VistaBlackjackTerminal, lleva 50% de progreso - Se creó la clase ControladorTerminal

--- src/main/java/com/example/practica1algoritmos/controlador/ControladorTerminal.java
+++ src/main/java/com/example/practica1algoritmos/controlador/ControladorTerminal.java
@@ -1,6 +1,7 @@
 package com.example.practica1algoritmos.controlador;
 
 import com.example.practica1algoritmos.modelo.blackjack.BlackjackGame;
+import com.example.practica1algoritmos.modelo.blackjack.Jugador;
 import com.example.practica1algoritmos.vista.VistaBlackjackTerminal;
 
 public class ControladorTerminal {
@@ -23,6 +24,23 @@ public class ControladorTerminal {
     public void realizarTurnos() {
         while (!juego.todosHanJugado()) {
             for (int i = 0; i < juego.getJugadores().size(); i++) {
+                Jugador j = juego.getJugadores().get(i);
+                int opcion = 0;
+                vista.mostrarMenu();
+                do {
+                    opcion = vista.leerNumeroEntero();
+                    switch (opcion) {
+                        case 1:
+                            juego.pedirCarta(i);
+                            break;
+                        case 2:
+                            juego.plantarApuesta(i);
+                            break;
+                        default:
+
+                            break;
+                    }
+                } while (opcion != 1 && opcion != 2);
 
             }
         }
