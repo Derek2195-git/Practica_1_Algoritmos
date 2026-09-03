311ea1c31caedceeef91e0a0094f12ed724d0b45
derek <112148047+Kerafyrm2415@users.noreply.github.com>
2026-09-02 22:20:17 -0700

    Mejore un poco lo que estaba haciendo hace rato en lo de redibujar al jugador, hora de hacer pruebas

--- src/main/java/com/example/practica1algoritmos/vista/gui/SecciónJugador.java
+++ src/main/java/com/example/practica1algoritmos/vista/gui/SecciónJugador.java
@@ -27,5 +27,10 @@ public class SecciónJugador {
         return contenedor;
     }
 
-    public void redibujar(int indiceJugador) {}
+    public void redibujar(int turnoActualJugador, boolean noEsDealer) {
+        for (int i = 0; i < panelesJugador.size(); i++) {
+            boolean turnoDeUnJugador = noEsDealer && i == turnoActualJugador;
+            panelesJugador.get(i).redibujar(turnoDeUnJugador);
+        }
+    }
 }
