eb3dd4fd4ff6210714a254603e7777846e385eb4
Derek2195-git <derek.garzon@uabc.edu.mx>
2026-09-16 15:47:09 -0700

    Se creó la rama undo

--- src/main/java/com/example/practica1algoritmos/modelo/blackjack/BlackjackGame.java
+++ src/main/java/com/example/practica1algoritmos/modelo/blackjack/BlackjackGame.java
@@ -105,7 +105,6 @@ public class BlackjackGame {
     }
 
     public void dealerPideUnaCarta() {
-
         dealer.pedirCarta(mazoCartas.obtenerUnaCarta());
         dealer.mostrarSusCartas();
     }
