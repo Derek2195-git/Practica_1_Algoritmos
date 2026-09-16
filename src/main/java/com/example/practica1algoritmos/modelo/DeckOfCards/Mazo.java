825ff64a96885fe1b106c452604fc0f3ca08058e
Derek2195-git <derek.garzon@uabc.edu.mx>
2026-09-16 14:42:25 -0700

    Se empezó el trabajo de reemplazar el ArrayList de la clase Mazo por una Pila

--- src/main/java/com/example/practica1algoritmos/modelo/DeckOfCards/Mazo.java
+++ src/main/java/com/example/practica1algoritmos/modelo/DeckOfCards/Mazo.java
@@ -5,31 +5,45 @@ package com.example.practica1algoritmos.modelo.DeckOfCards;
  * @author (Cecilia Curlango Rosas)
  * @version (2025-2)
  */
+import com.example.practica1algoritmos.modelo.Pila;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Mazo {
-    private ArrayList<CartaInglesa> cartas = new ArrayList<>();
+    private Pila<CartaInglesa> cartas;
 
     public Mazo() {
+         cartas = new Pila<>(52);
         llenar(); // crea todas las cartas, excluyendo Jokers
-        mezclar();
+        // mezclar();
     }
 
     /**
      * Obtiene todas las cartas del mazo.
-     * @return
+     * Aqui se van a aplicar modificaciones parecidas a la clase Mano para retornar las cartas
+     * @return ArrayList con todas las cartas que haya en el mazo.
      */
     public ArrayList<CartaInglesa> getCartas() {
-        return cartas;
+        Pila<CartaInglesa> pilaAux = new Pila<>(cartas.tamanoPila());
+        ArrayList<CartaInglesa> ordenOriginalCartas = new ArrayList<>();
+
+        while(!cartas.pilaVacia()) {
+            pilaAux.push(cartas.pop());
+        }
+        while(!pilaAux.pilaVacia()) {
+            CartaInglesa carta = pilaAux.pop();
+            cartas.push(carta);
+            ordenOriginalCartas.add(carta);
+        }
+        return ordenOriginalCartas;
     }
 
     public CartaInglesa obtenerUnaCarta() {
-        if (cartas.size() > 0) {
-            return cartas.remove(0);
-        }
-        return null;
+        return cartas.pop();
     }
+
+    // A lo mejor tendré que reescribir esto
     private void mezclar() {
         Collections.shuffle(cartas);
     }
